d02b2388f85229b6a9ed61ae2482408fe4b75db7
kremlevmax <kremlevmax1989@gmail.com>
2021-08-18 00:50:23 -0700

    created home page

---
+++ pages/index.js
@@ -0,0 +1,24 @@
+import MeetupList from "../components/meetups/MeetupList";
+
+const DUMMY_DATA = [
+  {
+    id: "m1",
+    image:
+      "https://s3.amazonaws.com/media.eremedia.com/wp-content/uploads/2017/10/17082809/meetup.jpg",
+    title: "First Meetup",
+    address: "Right here",
+  },
+  {
+    id: "m2",
+    image:
+      "https://s3.amazonaws.com/media.eremedia.com/wp-content/uploads/2017/10/17082809/meetup.jpg",
+    title: "Second Meetup",
+    address: "Right there",
+  },
+];
+
+const HomePage = () => {
+  return <MeetupList meetups={DUMMY_DATA} />;
+};
+
+export default HomePage;
